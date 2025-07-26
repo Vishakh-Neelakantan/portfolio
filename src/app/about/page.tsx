9b87c796daca33596b36e6d9f07b445c2f4c90da
Vishakh <vishakhneelakantan@gmail.com>
2025-07-26 18:37:14 +0530

    updt-july-3

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -255,7 +255,7 @@ export default function About() {
                         </Text>
                       ))}
                     </Column>
-                    {experience.images.length > 0 && (
+                    {/* {experience.images.length > 0 && (
                       <Flex fillWidth paddingTop="m" paddingLeft="40" wrap>
                         {experience.images.map((image, index) => (
                           <Flex
@@ -279,8 +279,8 @@ export default function About() {
                             />
                           </Flex>
                         ))}
-                      </Flex>
-                    )}
+                      </Flex> */}
+                    {/* )} */}
                   </Column>
                 ))}
               </Column>
